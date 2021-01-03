00679ee58c311adc42289fcfe3891f1c5c1287f0
mick-us <mickou147@gmail.com>
2021-01-03 20:59:20 +0100

    inscription fonctionne

--- Target.php
+++ Target.php
@@ -30,19 +30,20 @@ else {
     print("<h2> Vos informations :<br></h2>");
     print("$nom<br>$prenom<br>$ville<br>$email<br>$categorie<br>$type<br>$annonce<br>");
     echo'<br><a href="recherche.php">recherche</a><br>';
-}
-$hote = 'localhost';
-$login = 'root';
-$pass='';
-$namedb = 'badoovolat';
-try {
-    $connection = new PDO("mariadb:host=$hote;dbname=$namedb", $login, $pass);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'success in connecting to database';
-    $Sqlcode="INSERT INTO Personne VALUES('$nom','$prenom','$email','$categorie','$type','$annonce')";
-    $connection->exec($Sqlcode);
-} catch (PDOException $e) {
-    echo 'echec in connection:' . $e->getMessage();
+    $hote = 'localhost';
+    $login = 'root';
+    $pass='';
+    $dbport=3307;
+    $namedb = 'badoovolat';
+    try {
+        $connection = new PDO("mysql:host=$hote;port=$dbport;dbname=$namedb", $login, $pass);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'success in connecting to database';
+        $Sqlcode="INSERT INTO Personne(nom, prenom, ville, email, categorie, type, annonce) VALUES('$nom', '$prenom', '$ville', '$email', '$categorie', '$type', '$annonce')";
+        $connection->exec($Sqlcode);
+    } catch (PDOException $e) {
+        echo 'echec in connection:' . $e->getMessage();
+    }
 }
 ?> 
 </div>
